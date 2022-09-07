7128b88d3ad5e22cacba90a0ad40fed88e5c5e47
AndreiKorsunov <andrei.korshunov2004@gmail.com>
2022-09-07 15:48:13 +0300

    *update*

--- Harjutus_Klassid/Program.cs
+++ Harjutus_Klassid/Program.cs
@@ -14,33 +14,52 @@ namespace Harjutus_Klassid
         {
             List<Isik> people = new List<Isik>();
 
-            Tootaja tootaja = new Tootaja("Ash", 1990, Isik.Sugu.naine, "31204", "31204", "asd", "amet", 2000, 79, 1.89);
-            tootaja.printInfo();
+            Tootaja Ash = new Tootaja("Ash", 1990, Isik.Sugu.naine, "31204", "31204", "asd", "amet", 2000, 79, 1.89);
             Console.WriteLine();
 
-            Kutsekooliopilane kutsekooliopilane = new Kutsekooliopilane("Daniel", 1990, Isik.Sugu.mees, "12463", "12463", "Tartu Tehnikaülikool ", 3, "tarkvaraarendaja", "..", 91, 2.01);
-            kutsekooliopilane.printInfo();
+            Kutsekooliopilane Daniel = new Kutsekooliopilane("Daniel", 1990, Isik.Sugu.mees, "12463", "12463", "Tartu Tehnikaülikool ", 3, "tarkvaraarendaja", "..", 91, 2.01);
             Console.WriteLine();
 
-            Opilane opilane = new Opilane("Marta", 1990, Isik.Sugu.naine, "13521" ,"13521", "TLVL", 9, "humanitaar", 65, 1.85);
-            opilane.printInfo();
+            Opilane Marta = new Opilane("Marta", 1990, Isik.Sugu.naine, "13521" ,"13521", "TLVL", 9, "humanitaar", 65, 1.85);
             Console.WriteLine();
 
-            people.Add(tootaja);
-            people.Add(kutsekooliopilane);
-            people.Add(opilane);
+            people.Add(Ash);
+            people.Add(Daniel);
+            people.Add(Marta);
 
-            StreamWriter to_file = new StreamWriter(@"..\..\People.txt", false);
+            StreamWriter sw = new StreamWriter(@"..\..\People.txt", false);
             foreach (Isik p in people)
             {
                 p.printInfo();
-                to_file.WriteLine(p.nimi + ", " + p.arvutaVanus() + ", " + p.sugu + ";");
+                sw.WriteLine(p.nimi + ", " + p.arvutaVanus() + ", " + p.sugu + ", " + p.indeks + ", " + p.konna + ", " + p.kaal + ", " + p.pikkus + ";");
             }
-            to_file.Close();
-            StreamReader from_file = new StreamReader(@"..\..\People.txt");
-            List<string> lines = new List<string>();
-            int n = from_file.ReadToEnd().Split(new char[] { '\n' }).Length;
-            Console.WriteLine("-----" + n +"-----");
+            sw.Close();
+
+            List<Opilane> opilased = new List<Opilane>();
+            StreamReader sr = new StreamReader(@"..\..\People.txt");
+            string text;
+            while ((text = sr.ReadLine()) != null)
+            {
+                string[] rida = text.Split(' ');
+                opilased.Add(new Opilane(rida[0], int.Parse(rida[1]), TextFailistEnumSugu(rida[2]), rida[3], rida[4]));//, rida[5], int.Parse(rida[6]), rida[7] , double.Parse(rida[8]), double.Parse(rida[9])
+            }
+            sr.Close();
+            foreach(var opilane1 in opilased)
+            {
+                Console.WriteLine(opilane1.nimi + " " + opilane1.sugu);
+            }
+            Console.ReadLine();
+            Isik.Sugu TextFailistEnumSugu(string andmed)
+            {
+                switch (andmed)
+                {
+                    case "naine":
+                        return Isik.Sugu.naine;
+                    default:
+                        return Isik.Sugu.mees;
+                }
+            }
+
             Console.ReadKey();
         }
     }
